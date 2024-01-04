9c76100ec862ef37d1216b8a21937a1421b2547c
fverne <frederikverne95@gmail.com>
2024-01-04 13:32:00 +0100

    added wrapper to matmult_lib

--- src/matmult.c
+++ src/matmult.c
@@ -1,3 +1,5 @@
+#include "cblas.h"
+
 void
 matmult_nat(int m, int n, int k, double **A, double **B, double **C) {
     for(int i = 0; i < m; i++) {
@@ -12,7 +14,13 @@ matmult_nat(int m, int n, int k, double **A, double **B, double **C) {
 
 void
 matmult_lib(int m, int n, int k, double **A, double **B, double**C) {
-    // work here
+    double alpha = 1.0;
+    double beta = 0.0;
+    
+    cblas_dgemm(CblasRowMajor, 
+    CblasNoTrans,
+    CblasNoTrans,
+    m, n, k, alpha, *A, k, *B, n, beta, *C, n);
 }
 
 void 
