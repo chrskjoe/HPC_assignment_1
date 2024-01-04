7ec50a2e21105d15748f975c0a75890727eaa7e1
fverne <frederikverne95@gmail.com>
2024-01-04 11:30:04 +0100

    added header files and definitions

--- src/matmult.c
+++ src/matmult.c
@@ -9,3 +9,38 @@ matmult_nat(int m, int n, int k, double **A, double **B, double **C) {
         }  
     }  
 }
+
+void
+matmult_lib(int m, int n, int k, double **A, double **B, double**C) {
+    // work here
+}
+
+void 
+matmult_mnk(int m,int n,int k,double **A,double **B,double **C) {
+    // work here
+}
+
+void 
+matmult_mkn(int m,int n,int k,double **A,double **B,double **C) {
+    // work here
+}
+
+void 
+matmult_nmk(int m,int n,int k,double **A,double **B,double **C) {
+    // work here
+}
+
+void 
+matmult_nkm(int m,int n,int k,double **A,double **B,double **C) {
+    // work here
+}
+
+void 
+matmult_kmn(int m,int n,int k,double **A,double **B,double **C) {
+    // work here
+}
+
+void 
+matmult_knm(int m,int n,int k,double **A,double **B,double **C) {
+    // work here
+}
