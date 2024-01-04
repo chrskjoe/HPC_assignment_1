9fa32d549d5c2ab8f179282bbfe6b605c42da201
fverne <frederikverne95@gmail.com>
2024-01-04 13:42:23 +0100

    added permutations

--- src/matmult.c
+++ src/matmult.c
@@ -2,21 +2,14 @@
 
 void
 matmult_nat(int m, int n, int k, double **A, double **B, double **C) {
-    for(int i = 0; i < m; i++) {
-        for(int j = 0; j < n; j++) {
-            C[i][j] = 0;
-            for(int l = 0; l < k; l++) {
-                C[i][j] += A[i][l] * B[l][j];
-            }
-        }  
-    }  
+    matmult_mnk(m, n, k, A, B, C);
 }
 
 void
 matmult_lib(int m, int n, int k, double **A, double **B, double**C) {
     double alpha = 1.0;
     double beta = 0.0;
-    
+
     cblas_dgemm(CblasRowMajor, 
     CblasNoTrans,
     CblasNoTrans,
@@ -25,32 +18,74 @@ matmult_lib(int m, int n, int k, double **A, double **B, double**C) {
 
 void 
 matmult_mnk(int m,int n,int k,double **A,double **B,double **C) {
-    // work here
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            C[i][j] = 0;
+            for(int l = 0; l < k; l++) {
+                C[i][j] += A[i][l] * B[l][j];
+            }
+        }  
+    } 
 }
 
 void 
 matmult_mkn(int m,int n,int k,double **A,double **B,double **C) {
-    // work here
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < k; j++) {
+            C[i][j] = 0;
+            for(int l = 0; l < n; l++) {
+                C[i][j] += A[i][l] * B[l][j];
+            }
+        }  
+    } 
 }
 
 void 
 matmult_nmk(int m,int n,int k,double **A,double **B,double **C) {
-    // work here
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            C[i][j] = 0;
+            for(int l = 0; l < k; l++) {
+                C[i][j] += A[i][l] * B[l][j];
+            }
+        }  
+    } 
 }
 
 void 
 matmult_nkm(int m,int n,int k,double **A,double **B,double **C) {
-    // work here
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < k; j++) {
+            C[i][j] = 0;
+            for(int l = 0; l < m; l++) {
+                C[i][j] += A[i][l] * B[l][j];
+            }
+        }  
+    } 
 }
 
 void 
 matmult_kmn(int m,int n,int k,double **A,double **B,double **C) {
-    // work here
+    for(int i = 0; i < k; i++) {
+        for(int j = 0; j < m; j++) {
+            C[i][j] = 0;
+            for(int l = 0; l < n; l++) {
+                C[i][j] += A[i][l] * B[l][j];
+            }
+        }  
+    } 
 }
 
 void 
 matmult_knm(int m,int n,int k,double **A,double **B,double **C) {
-    // work here
+    for(int i = 0; i < k; i++) {
+        for(int j = 0; j < n; j++) {
+            C[i][j] = 0;
+            for(int l = 0; l < m; l++) {
+                C[i][j] += A[i][l] * B[l][j];
+            }
+        }  
+    } 
 }
 
 void 
