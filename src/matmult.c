9e83153d3402f24e059a40287b7fa3532ee44978
fverne <frederikverne95@gmail.com>
2024-01-05 14:14:40 +0100

    improved cblass formatting

--- src/matmult.c
+++ src/matmult.c
@@ -22,10 +22,10 @@ matmult_lib(int m, int n, int k, double **A, double **B, double**C) {
     double alpha = 1.0;
     double beta = 0.0;
 
-    cblas_dgemm(CblasRowMajor, 
-    CblasNoTrans,
-    CblasNoTrans,
-    m, n, k, alpha, *A, k, *B, n, beta, *C, n);
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
+    m, n, k, 
+    alpha, *A, k, 
+    *B, n, beta, *C, n);
 }
 
 void 
