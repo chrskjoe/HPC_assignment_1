1158f5befab5f4da42b113ea60a305f316b409bc
Chang-Rong Xue <changrong.xue@hotmail.com>
2024-01-06 01:20:47 +0100

    feat(plot): add cache indicators in x-axis

--- mhplot.py
+++ mhplot.py
@@ -43,6 +43,7 @@ if __name__ == "__main__":
     parser.add_argument('-L3', default=25600, help="Size of L3 Cache with unut of KB")
     parser.add_argument('-log', action="store_true", help="Draw the log of the data")
     args = parser.parse_args()
+    caches = list(zip(['L1', 'L2', 'L3'], [args.L1, args.L2, args.L3]))
 
     # read the data file and plot
     for file, label in zip(args.files, args.labels):
@@ -54,12 +55,21 @@ if __name__ == "__main__":
 
     plt.xlabel('Memory [kBytes]')
     plt.ylabel('Performance [Mflops/s]')
-    plt.axvline(x=args.L1, color='r', linestyle='--', linewidth=1, label="L1 cache")
-    plt.axvline(x=args.L2, color='r', linestyle='--', linewidth=1, label="L2 cache")
-    plt.axvline(x=args.L3, color='r', linestyle='--', linewidth=1, label="L3 cache")
-    plt.grid()
+    for label, cache in caches:
+        plt.axvline(x=cache, color='r', linestyle='--', linewidth=1, label=label)
+    
     if args.log: 
         plt.xscale('log')
+    
+    xlim = plt.xlim()
+    plt.savefig(args.name) # https://stackoverflow.com/questions/68558593/how-to-get-tick-labels-in-matplotlib-prior-to-calling-plt-show
+    plt.xticks(
+        ticks= list(plt.gca().get_xticks()) + [cache for _, cache in caches], 
+        labels= plt.gca().get_xticklabels(minor=False) + [label for label, _ in caches]
+    )
+    plt.gca().set_xlim(xlim)
+
+    plt.grid()
     plt.legend(args.labels)
     plt.title('Perfomance Matrix Multiplication')
     # plt.show()
